13c04198ca4c3b59797e8717919c62f9ea6922c9
Madhurmeet Milind Jadhav <89741221+MadhurmeetJadhav@users.noreply.github.com>
2023-11-09 00:46:03 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
+import store, {persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import RootNavigator from './navigation/RootNavigator';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default App;
